b2bbf960e586e14e1e14506b620fc6fd1fcefdc1
Adrian Webb <adrian@webb.sh>
2021-07-03 21:56:59 -0400

    Adding initial ec2 server provider start and stop method implementations.

--- plugins/server/ec2.py
+++ plugins/server/ec2.py
@@ -15,3 +15,26 @@ class Provider(BaseProvider('server', 'ec2')):
     def finalize_terraform(self, instance):
         super().finalize_terraform(instance)
         self.delete_aws_ec2_keypair(instance.subnet.network, instance.config['key_name'])
+
+
+    def start(self):
+        instance = self.check_instance('server start')
+        instance_id = instance.variables['instance_id']
+        ec2 = self.ec2(instance.subnet.network)
+
+        ec2.start_instances(InstanceIds = [instance_id])
+        waiter = ec2.get_waiter('instance_running')
+        waiter.wait(InstanceIds = [instance_id])
+
+        self.command.success("Server {} has been started".format(instance.name))
+
+    def stop(self):
+        instance = self.check_instance('server stop')
+        instance_id = instance.variables['instance_id']
+        ec2 = self.ec2(instance.subnet.network)
+
+        ec2.stop_instances(InstanceIds = [instance_id])
+        waiter = ec2.get_waiter('instance_stopped')
+        waiter.wait(InstanceIds = [instance_id])
+
+        self.command.success("Server {} has been stopped".format(instance.name))
